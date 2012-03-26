d2e88c693ea9aad79379c1e2709c1b1af2565758
Joe Hirn <joseph.hirn@gmail.com>
2012-03-26 00:31:59 -0500

    Moved things from custom.el into personal.el. Changed aspell default dictionary.

--- personal/personal.el
+++ personal/personal.el
@@ -9,13 +9,11 @@
   (interactive)
   (switch-to-buffer (other-buffer)))
 
-(global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-/") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-S-f") 'prelude-indent-buffer)
 (global-set-key (kbd "C-:") 'switch-to-previous-buffer)
 
 
-;; Add my personal figs
 (set-frame-parameter (selected-frame) 'alpha '(94 76))
 (menu-bar-mode 1)
 (global-linum-mode 1)
@@ -27,9 +25,11 @@
   (set-frame-position (selected-frame) 0 0)
   (set-frame-size (selected-frame) 1000 1000))
 
-(setq ispell-dictionary "English")
+(setq ispell-dictionary "en")
 
 (require 'rspec-mode)
+(setq rspec-spec-command "rspec"
+      rspec-use-rake-flag nil)
 
 ;; rbenv
 (setq exec-path (cons "~/.rbenv/bin" exec-path))
@@ -40,3 +40,14 @@
 (add-hook 'before-save-hook 'whitespace-cleanup nil t)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
+
+(setq js-indent-level 2)
+
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
