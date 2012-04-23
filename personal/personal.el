6fa994dde564489e445b66a02912f97bc95fe3c5
Joe Hirn <joseph.hirn@gmail.com>
2012-04-23 11:10:46 -0500

    Rakefiles, gemspecs added to ruby mode

--- personal/personal.el
+++ personal/personal.el
@@ -1,6 +1,7 @@
+;Apple, think different
 (setq
- ns-command-modifier   'meta       ; Apple/Command key is Meta
- ns-alternate-modifier 'super      ; Option is the Mac Option key
+ ns-command-modifier   'meta            ; Apple/Command key is Meta
+ ns-alternate-modifier 'super           ; Option is the Mac Option key
  mouse-wheel-scroll-amount '(1)
  mouse-wheel-progressive-speed nil
  )
@@ -29,17 +30,20 @@
 (setq default-tab-width 2)
 (setq js-indent-level 2) ;nerd rage I must set this separate from 'default-tab-width
 
-; redundant comment about rspec
-(require 'rspec-mode)
-(define-key rspec-mode-verifible-keymap (kbd "s") 'rspec-verify-single)
-(setq rspec-spec-command "rspec"
-      rspec-use-rake-flag nil)
-
-;; rbenv
+;; Ruby
 (setq exec-path (cons "~/.rbenv/bin" exec-path))
 (setenv "PATH" (concat "~/.rbenv/bin:" (getenv "PATH")))
 (setq exec-path (cons "~/.rbenv/shims" exec-path))
 (setenv "PATH" (concat "~/.rbenv/shims:" (getenv "PATH")))
+
+(require 'rspec-mode)
+(define-key rspec-mode-verifible-keymap (kbd "s") 'rspec-verify-single)
+(setq rspec-spec-command "rspec"
+      rspec-use-rake-flag nil)
+(add-to-list 'auto-mode-alist '("\\.rake" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec" . ruby-mode))
+
 
 ; Captain hooks
 (add-hook 'before-save-hook 'whitespace-cleanup nil t)
@@ -51,12 +55,10 @@
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode t)))
 (add-hook 'scheme-mode-hook           (lambda () (paredit-mode t)))
 
-
-
+;Smexy
 (smex-initialize)
 (smex-auto-update 30)
 (setq smex-show-unbound-commands t)
-
 
 
 (global-set-key (kbd "M-x") 'smex)
